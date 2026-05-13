4d7be283e3f93e6a15addf0bab7896f596ea361a
[dyad] <jacksonstorr97@gmail.com>
2026-05-12 20:45:58 -0400

    [dyad] wrote 1 file(s)

---
+++ src/components/SomeComponent.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
+
+const SomeComponent = () => {
+  const [count, setCount] = useState(0);
+  // ... rest of the component
+};
